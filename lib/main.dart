1fb287890b3e5f7d4d75abbd60afba700c36631c
AayushPandeyy <ap200061@gmail.com>
2025-05-24 09:41:29 +0545

    add: Added bottom navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:jersey_ecommerce/HomePage.dart';
+import 'package:jersey_ecommerce/NavigationScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: HomePage(),
+      home: NavigationScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
